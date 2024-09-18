10951e2a461e4bd85f6151d16dd699fe7dcf1f40
KatieSrbk <miss.kate.sh@gmail.com>
2024-09-18 21:53:54 +0300

    implementation of chakra-ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react'
+import theme from './styles/theme'
 import './App.css'
 
 function App() {
-  return <></>
+  return (
+    <ChakraProvider>
+      <ChakraBaseProvider theme={theme}>
+        <div></div>
+      </ChakraBaseProvider>
+    </ChakraProvider>
+  )
 }
 
 export default App
