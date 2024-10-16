e8eac34b58e2347ca1144eb0d9870c4156c3427b
KatieSrbk <miss.kate.sh@gmail.com>
2024-10-16 19:47:15 +0300

    add content and styles to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,45 @@
-import { Flex } from '@chakra-ui/react'
+import { Button, Flex, Image, Link, Text } from '@chakra-ui/react';
+
+const navBtns = [
+  { text: 'Картинка клуба' },
+  { text: 'Home' },
+  { text: 'About Us' },
+  { text: 'Тренировочный план' },
+  { text: 'Соревнования' },
+  { text: 'Мерч' },
+  { text: 'Логин' },
+  { text: 'Зарегестрироватся' },
+];
 
 export const Header = () => {
-  return <Flex>Header</Flex>
-}
+  return (
+    <Flex flexDirection="column">
+      <Flex bg="#4faaff" height="58px" width="100%" alignItems="center">
+        <Flex flexGrow={1} justifyContent="center">
+          <Text color="white" fontWeight="500" fontSize="15px">
+            ОФИЦИАЛЬНЫЙ САЙТ СПОРТИВНОГО КЛУБА "ЛЕГЕНДА" ГОРОДА ТАГАНРОГА
+          </Text>
+        </Flex>
+        <Flex gap="11px" width="140px" flexBasis="17%">
+          <Link>
+            <Image src="/icons/telegram.svg" alt="" />
+          </Link>
+          <Link>
+            <Image src="/icons/vk.svg" alt="" />
+          </Link>
+          <Link>
+            <Image src="/icons/instagram.svg" alt="" />
+          </Link>
+          <Link>
+            <Image src="/icons/gmail.svg" alt="" />
+          </Link>
+        </Flex>
+      </Flex>
+      <Flex justifyContent="center">
+        {navBtns.map((item) => (
+          <Button variant="headerNav">{item.text}</Button>
+        ))}
+      </Flex>
+    </Flex>
+  );
+};
---
+++ src/styles/components/button.ts
@@ -0,0 +1,23 @@
+export const Button = {
+  variants: {
+    headerNav: {
+      bg: 'transparent',
+      height: '56px',
+      position: 'relative',
+      _after: {
+        content: '""',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        height: '3.5px',
+        bg: 'blue.500',
+        opacity: 0, // Скрыто по умолчанию
+        transition: 'opacity 0.3s', // Плавный переход
+      },
+      _hover: {
+        _after: { opacity: 1 }, // Появляется при наведении
+      },
+    },
+  },
+};
